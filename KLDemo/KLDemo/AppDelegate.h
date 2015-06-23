a84827ae5775f45ce39a30030239e4fe0c95a67e
啃梨 <ggggqqqeeq@qq.com>
2015-06-23 10:03:58 +0800

    no message

---
+++ KLDemo/KLDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KLDemo
+//
+//  Created by 啃梨 on 15/6/23.
+//  Copyright (c) 2015年 啃梨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KLDemo/KLDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KLDemo
+//
+//  Created by 啃梨 on 15/6/23.
+//  Copyright (c) 2015年 啃梨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
